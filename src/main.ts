3f2e802c4fe0b8bef0f401b52d68b774b4d9862c
Prem <premkumar@divami.com>
2025-05-23 20:22:07 +0530

    Added Config, exception filters, global validation pipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ServerConfig } from './config/server.config';
+import { ExceptionsHandler } from '@nestjs/core/exceptions/exceptions-handler';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.useGlobalFilters(new ExceptionsHandler());
+  await app.listen(ServerConfig.APP_PORT);
 }
 bootstrap();
